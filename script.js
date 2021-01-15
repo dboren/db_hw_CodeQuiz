3823bf0d8549be97b87f91bd966766cb3d9e2eb4
dboren <danielboren@att.net>
2021-01-15 00:44:05 -0600

    Added explanatory text to launch page

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ var highScoresLinkText = document.createTextNode("View high scores");
 var timerEl = document.createElement("p");
 var timerText = document.createTextNode("Seconds remaining:" + secondsLeft);
 
+var launchCentral = document.createElement("div");
+var launchHeader = document.createElement("h1");
+var launchHeaderText = document.createTextNode("Quick Coding Quiz");
+
+var explainerEl = document.createElement("p");
+var explainerText = document.createTextNode("Click the start button to begin. You will have 100 seconds to complete the quiz. Additional time will be subtracted for incorrect answers.");
+
 function init() {
     document.body.appendChild(pageHeader);
     pageHeader.appendChild(highScoresLink);
@@ -16,6 +23,15 @@ function init() {
 
     pageHeader.appendChild(timerEl);
     timerEl.appendChild(timerText);
+
+    document.body.appendChild(launchCentral);
+    launchCentral.appendChild(launchHeader);
+    launchHeader.appendChild(launchHeaderText);
+    launchCentral.appendChild(explainerEl);
+    explainerEl.appendChild(explainerText);
+
+    
+
 
 
     };
