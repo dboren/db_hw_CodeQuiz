a5abc14252e84afa25fcfcc8ea399bdcd1c79d5a
dboren <danielboren@att.net>
2021-01-15 00:15:30 -0600

    added global score and secondsLeft vars, wrote init function to create 'View high scores' link on page

---
+++ script.js
@@ -0,0 +1,15 @@
+var currentScore = 0;
+var secondsLeft = 100;
+
+var pageHeader = document.createElement("div");
+var highScoresLink = document.createElement("A");
+var highScoresLinkText = document.createTextNode("View high scores");
+
+function init() {
+    document.body.appendChild(pageHeader);
+    pageHeader.appendChild(highScoresLink);
+    highScoresLink.setAttribute("href", "HighScores.html");
+    highScoresLink.appendChild(highScoresLinkText);
+    };
+
+init();
