85efda0230b40de7f8b19df1141b1d0a431923e2
dboren <danielboren@att.net>
2021-01-15 00:28:04 -0600

    modified init function to create timer onscreen

--- script.js
+++ script.js
@@ -5,11 +5,19 @@ var pageHeader = document.createElement("div");
 var highScoresLink = document.createElement("A");
 var highScoresLinkText = document.createTextNode("View high scores");
 
+var timerEl = document.createElement("p");
+var timerText = document.createTextNode("Seconds remaining:" + secondsLeft);
+
 function init() {
     document.body.appendChild(pageHeader);
     pageHeader.appendChild(highScoresLink);
     highScoresLink.setAttribute("href", "HighScores.html");
     highScoresLink.appendChild(highScoresLinkText);
+
+    pageHeader.appendChild(timerEl);
+    timerEl.appendChild(timerText);
+
+
     };
 
 init();
